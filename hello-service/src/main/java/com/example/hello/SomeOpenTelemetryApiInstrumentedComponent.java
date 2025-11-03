126d94bcd82441e540540cc25287226eca7cb097
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-11-03 13:12:14 +0100

    Show how spans from OpenTelemetry API work together with Micrometer Tracing

--- hello-service/src/main/java/com/example/hello/SomeOpenTelemetryApiInstrumentedComponent.java
+++ hello-service/src/main/java/com/example/hello/SomeOpenTelemetryApiInstrumentedComponent.java
@@ -1,8 +1,15 @@
 package com.example.hello;
 
+import java.util.Random;
+
 import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.metrics.MeterProvider;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.TracerProvider;
+import io.opentelemetry.context.Scope;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,13 +22,52 @@ class SomeOpenTelemetryApiInstrumentedComponent {
 
     private final LongCounter counter;
 
-    SomeOpenTelemetryApiInstrumentedComponent(MeterProvider meterProvider) {
+    private final Tracer tracer;
+
+    private final Random random = new Random();
+
+    SomeOpenTelemetryApiInstrumentedComponent(MeterProvider meterProvider, TracerProvider tracerProvider) {
         Meter meter = meterProvider.get(getClass().getName());
+        this.tracer = tracerProvider.get(getClass().getName());
         this.counter = meter.counterBuilder("dummy-counter").build();
     }
 
-    @Scheduled(fixedRateString = "1s")
+    @Scheduled(fixedDelayString = "10s")
     private void run() {
         this.counter.add(1);
+        Span span = this.tracer.spanBuilder("dummy-span").startSpan();
+        try (Scope _ = span.makeCurrent()) {
+            sleep();
+            span.setStatus(StatusCode.OK);
+        } catch (RuntimeException ex) {
+            span.recordException(ex);
+            span.setStatus(StatusCode.ERROR);
+        } finally {
+            span.end();
+        }
+    }
+
+    private void sleep() {
+        if (this.random.nextBoolean()) {
+            throw new RuntimeException("Boom");
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException("Got interrupted", ex);
+        }
+    }
+
+    /**
+     * If this is called from e.g. HelloService, it also contributes to the current running span started by
+     * Micrometer Tracing.
+     */
+    public void doSomething() {
+        Span span = this.tracer.spanBuilder("do-something").startSpan();
+        try (Scope _ = span.makeCurrent()) {
+            span.setStatus(StatusCode.OK);
+        } finally {
+            span.end();
+        }
     }
 }
